fac79cd4f9565e6b52737b5c7a553ef8b7f01d48
mluzarow <ma.luzarow@gmail.com>
2019-06-02 17:52:22 -0500

    Hid delete btn via css.

--- src/Layers/LayerBuilder.js
+++ src/Layers/LayerBuilder.js
@@ -1,7 +1,19 @@
 class LayerBuilder {
 	buildLayerHTML (layerObj, deleteEvent, dragEvent, flagActive = false, flagDelete = true, ) {
 		let layer = document.createElement ("div");
-		layer.classList = "layer" + (!flagActive ? '' : " active");
+		let classes = [];
+		
+		classes.push ("layer");
+		
+		if (flagActive === true) {
+			classes.push ("active");
+		}
+		
+		if (flagDelete === true) {
+			classes.push ("deletable");
+		}
+		
+		layer.classList = classes.join (' ');
 		layer.dataset.layer_id = layerObj.id;
 		
 		let activeBar = document.createElement ("div");
@@ -9,14 +21,12 @@ class LayerBuilder {
 		
 		layer.appendChild (activeBar);
 		
-		if (flagDelete === true) {
-			let btnDelete = document.createElement ("div");
-			btnDelete.classList = "btn_delete";
-			btnDelete.innerHTML = 'X';
-			btnDelete.addEventListener ("click", () => deleteEvent (layerObj.id));
-			
-			layer.appendChild (btnDelete);
-		}
+		let btnDelete = document.createElement ("div");
+		btnDelete.classList = "btn_delete";
+		btnDelete.innerHTML = 'X';
+		btnDelete.addEventListener ("click", () => deleteEvent (layerObj.id));
+		
+		layer.appendChild (btnDelete);
 		
 		let name = document.createElement ("div");
 		name.classList = "name";
